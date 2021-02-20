146debb8b1218de285d25fecb8b27025e9f2b5b5
tommy-zitaoy <67605608+tommy-zitaoy@users.noreply.github.com>
2021-02-20 02:07:14 -0800

    abc

--- mapentities.js
+++ mapentities.js
@@ -181,7 +181,7 @@ class Golem extends Agent {
 					}
 					if (that.lastHit instanceof Ground) {
 						that.game.camera.score += 100;
-						if (that.hitWallCount < 5) {
+						if (that.hitWallCount < 7) {
 							that.game.knight.combo++;
 							if (that.game.knight.combo > that.game.camera.highestCombo) {
 								that.game.camera.highestCombo = that.game.knight.combo;
